d000096281c697b0bdc5eba8b5f3670f582906a4
MarcoA-Dev10 <marcoaurelioprg1@gmail.com>
2025-01-02 23:26:26 -0300

    Meu-Conversor-de-Moedas

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+
+function convertValues() {
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyvaluetoconvert = document.querySelector(".currency-value-to-convert")
+    const currencyvalueconverted = document.querySelector(".currency-value-converted")
+
+
+    const dolarToday = 6.15
+    const euroToday = 6.30
+    const libraToday = 7.10
+
+
+
+    if (currencySelect.value == "dolar") {
+        currencyvalueconverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyvalueconverted.innerHTML = new Intl.NumberFormat("ed-ED", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+    if (currencySelect.value == "libra") {
+        currencyvalueconverted.innerHTML = new Intl.NumberFormat("GBP", {
+            style:"currency",
+            currency:"GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
+
+    currencyvaluetoconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+}
+
+
+convertButton.addEventListener("click", convertValues)
